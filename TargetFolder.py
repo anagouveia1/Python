8e942b94482dcb6e7c645df983ab0132cdac96a1
Ana Gouveia <arg928@g.harvard.edu>
2017-09-05 18:38:09 -0400

    Add py files via upload

---
+++ TargetFolder.py
@@ -0,0 +1,10 @@
+#Designing a Little Program in CMD First:
+
+#Greet User With Access Function (python)
+#Copy Files While Calling Out #Inquiry About Target Folder with path.bat in Opt\Python
+
+
+#Program to create multiple sfolders
+
+#Text2Folder | Excel and VBA macro
+
---
+++ path.bat
@@ -0,0 +1,5 @@
+@echo off
+set /p id=Enter Source Path: 
+
+REM Directory:
+CD /d %id%
